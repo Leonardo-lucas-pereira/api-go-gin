6553c647d6cfd16d785dc70d62c6460f7e7c9d97
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-09-03 18:10:07 -0300

    Get: Exibir todos alunos

--- main.go
+++ main.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"github.com/Leonardo-lucas-pereira/api-go-gin/database"
-	"github.com/Leonardo-lucas-pereira/api-go-gin/models"
 	"github.com/Leonardo-lucas-pereira/api-go-gin/routes"
 )
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "Leonardo", CPF: "14258714587", RG: "42005479"},
-		{Nome: "Ana", CPF: "14248514587", RG: "92005439"},
-	}
-
 	routes.HandleRequests()
 }
