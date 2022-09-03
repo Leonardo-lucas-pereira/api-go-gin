7f711833220f796afd5b9423951db3d48b8166d7
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-09-03 12:57:48 -0300

    Models: Aluno.

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
-import "github.com/Leonardo-lucas-pereira/api-go-gin/routes"
+import (
+	"github.com/Leonardo-lucas-pereira/api-go-gin/models"
+	"github.com/Leonardo-lucas-pereira/api-go-gin/routes"
+)
 
 func main() {
+	models.Alunos = []models.Aluno{
+		{Nome: "Leonardo", CPF: "14258714587", RG: "42005479"},
+		{Nome: "Ana", CPF: "14248514587", RG: "92005439"},
+	}
 
 	routes.HandleRequests()
-
 }
