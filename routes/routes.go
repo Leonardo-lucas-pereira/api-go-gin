38584c5f3209b72bc33e4616d2c76d63899added
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-09-08 19:18:11 -0300

    fix routes

--- routes/routes.go
+++ routes/routes.go
@@ -7,12 +7,12 @@ import (
 
 func HandleRequests() {
 	r := gin.Default()
-	r.GET("/alunos", controllers.ExibeTodosAlunos)
-	r.GET("/:nome", controllers.Saudacao)
-	r.POST("/alunos", controllers.CriaNovoAluno)
-	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
-	r.DELETE("/alunos/:id", controllers.DeletaAluno)
-	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	r.GET("/alunos", controllers.TodosAlunos)
+	r.GET("/:nome", controllers.Saudacoes)
+	r.POST("/alunos", controllers.CriarNovoAluno)
+	r.GET("/alunos/:id", controllers.BuscarAlunoPorID)
+	r.DELETE("/alunos/:id", controllers.DeletarAluno)
+	r.PATCH("/alunos/:id", controllers.EditarAluno)
 	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
 	r.Run()
 }
